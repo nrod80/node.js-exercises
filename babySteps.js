d5447056928dbf398647f80b2593fd2ae377b072
nrod80 <nicholas.rodriguez@duke.edu>
2016-07-15 12:55:29 -0500

    final submission

--- babySteps.js
+++ babySteps.js
@@ -1,11 +1,11 @@
 var processArray = process.argv;
 
-	function processSum(processArray) {
-		var sum = 0;
-		for(i = 2; i < process.argv.length; i++) {
-			sum += Number(process.argv[i]);
-		}
-		console.log(sum);
+function processSum(processArray) {
+	var sum = 0;
+	for(i = 2; i < process.argv.length; i++) {
+		sum += Number(process.argv[i]);
 	}
+	console.log(sum);
+}
 
-	processSum(processArray);
+processSum(processArray);
--- myFirstIO.js
+++ myFirstIO.js
@@ -1,19 +1,19 @@
-#3 var fs = require('fs');
+var fs = require('fs');
 
-	var filePath = process.argv[2];
+var filePath = process.argv[2];
 
-	var buf = fs.readFileSync(filePath);
+var buf = fs.readFileSync(filePath);
 
-	function newlines(buf) {
-		var count = 0;
-		for(i = 0; i < buf.length; i++) {
-			if(buf[i] === '\n') {
-				count++;
-			}
+function newlines(buf) {
+	var count = 0;
+	for(i = 0; i < buf.length; i++) {
+		if(buf[i] === '\n') {
+			count++;
 		}
-		console.log(count);
 	}
+	console.log(count);
+}
 
-	newlines(buf.toString().split(""));
+newlines(buf.toString().split(""));
 
-console.log(buf.toString().split(""));
+// console.log(buf.toString().split(""));
